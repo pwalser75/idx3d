0cb6b6a93c2bcae550b6c8239d371ec39250067b
Peter Walser <peter.walser@ti8m.ch>
2026-09-11 11:48:51 +0200

    Remove legacy credits block from engine source headers
    Drop the 'Thanx & greetinx go to:' acknowledgements and the trailing
    author/contact lines from all 34 idx3d-core source files, keeping the
    copyright and usage header intact.

--- idx3d-core/src/main/java/idx3d/implicit/MarchingCubes.java
+++ idx3d-core/src/main/java/idx3d/implicit/MarchingCubes.java
@@ -9,29 +9,6 @@
 // | non-commercial purposes (of course on your own risk).
 // | If you intend to use idx3d for commercial purposes, please
 // | contact me with an e-mail [proxima@active.ch].
-// |
-// | Thanx & greetinx go to:
-// | * Wilfred L. Guerin, 	for testing, bug report, and tons 
-// |			of brilliant suggestions
-// | * Sandy McArthur,	for reverse loops
-// | * Dr. Douglas Lyons,	for mentioning idx3d1 in his book
-// | * Hugo Elias,		for maintaining his great page
-// | * the comp.graphics.algorithms people, 
-// | 			for scientific concerns
-// | * Tobias Hill,		for inspiration and awakening my
-// |			interest in java gfx coding
-// | * Kai Krause,		for inspiration and hope
-// | * Incarom & Parisienne,	for keeping me awake during the 
-// |			long coding nights
-// | * Doris Langhard,	for being the sweetest girl on earth
-// | * Etnica, Infinity Project, X-Dream and "Space Night"@BR3
-// | 			for great sound while coding
-// | and all coderz & scenerz out there (keep up the good work, ppl :)
-// |
-// | Peter Walser
-// | proxima@active.ch
-// | http://www2.active.ch/~proxima
-// | "On the eigth day, God started debugging"
 // | -----------------------------------------------------------------
 
 // | NOTE:
